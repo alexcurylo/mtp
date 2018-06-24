5ae7f8ba671cdb95263e1110d1f3b05af8f9193b
Alex Curylo <alex@alexcurylo.com>
2018-06-24 13:22:22 +0700

    Xcode 9.4.1 tabbed app template

---
+++ MTP/MTP/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  MTP
+//
+//  Created by Alex Curylo on 6/24/18.
+//  Copyright © 2018 Trollwerks Inc. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
