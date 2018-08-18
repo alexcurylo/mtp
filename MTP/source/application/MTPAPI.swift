d03f213278c6464c355c66e20718ce6790f62845
Alex Curylo <alex@alexcurylo.com>
2018-08-18 16:43:39 +0700

    Canonical NSURLSession handler

--- MTP/source/application/MTPAPI.swift
+++ MTP/source/application/MTPAPI.swift
@@ -16,7 +16,6 @@ enum MTPAPI {
         then(true)
     }
 
-    // swiftlint:disable:next function_body_length
     static func login(email: String,
                       password: String,
                       then: @escaping (Bool) -> Void) {
@@ -25,57 +24,50 @@ enum MTPAPI {
             return then(false)
         }
 
-        // server endpoint
         let endpoint = "https://mtp.travel/api/user/login"
         guard let endpointUrl = URL(string: endpoint) else {
             return then(false)
         }
         let config = URLSessionConfiguration.default
-        let session = URLSession(configuration: config)
+        let session = URLSession(configuration: config, delegate: nil, delegateQueue: nil)
 
-        //Make JSON to send to send to server
-        var json = [String: Any]()
-
-        json["email"] = email
-        json["password"] = password
+        let json: [String: Any] = [
+            "email": email,
+            "password": password
+        ]
 
         do {
             let data = try JSONSerialization.data(withJSONObject: json, options: [])
 
-            var request = URLRequest(url: endpointUrl)
+            var request = URLRequest(url: endpointUrl,
+                                     cachePolicy: .reloadIgnoringLocalAndRemoteCacheData,
+                                     timeoutInterval: 10.0 * 1_000)
             request.httpMethod = "POST"
             request.httpBody = data
-            request.addValue("application/json", forHTTPHeaderField: "Content-Type")
-            request.addValue("application/json", forHTTPHeaderField: "Accept")
+            request.addValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+            request.addValue("application/json; charset=utf-8", forHTTPHeaderField: "Accept")
 
             let task = session.dataTask(with: request) { data, _, error in
                 guard error == nil else {
                     print("error calling POST on /user/login: \(String(describing: error))")
                     return then(false)
                 }
-                guard let responseData = data else {
-                    print("Error: did not receive data")
+                guard let responseData = data,
+                      let userData = try? JSONSerialization.jsonObject(with: responseData, options: []),
+                      let userDict = userData as? [String: Any] else {
+                    print("Could not get JSON from responseData as dictionary")
                     return then(false)
                 }
-
-                do {
-                    guard let userData = try JSONSerialization.jsonObject(with: responseData,
-                                                                          options: []) as? [String: Any] else {
-                        print("Could not get JSON from responseData as dictionary")
-                        return
-                    }
-                    print("The userData is: " + userData.description)
-                    if let token = userData["token"] as? String {
-                        print("The token is: \(token)")
-                        UserDefaults.standard.token = token
-                        return then(true)
-                    }
-                } catch {
-                    print("error parsing response from POST on /login")
+                print("The userData is: " + userDict.description)
+                if let token = userDict["token"] as? String {
+                    print("The token is: \(token)")
+                    UserDefaults.standard.token = token
+                    return then(true)
                 }
                 return then(false)
             }
             task.resume()
+            session.finishTasksAndInvalidate()
         } catch {
             log.info("Error on login: \(email), \(password)")
             return then(false)
