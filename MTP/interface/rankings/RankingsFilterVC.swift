5eb84516d920f46bfea0ede97c15ec0444d7a450
Alex Curylo <alex@alexcurylo.com>
2018-10-23 10:50:26 +0700

    Add RankingsFilterVC

---
+++ MTP/interface/rankings/RankingsFilterVC.swift
@@ -0,0 +1,52 @@
+// @copyright Trollwerks Inc.
+
+import UIKit
+
+final class RankingsFilterVC: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+
+        configure()
+    }
+
+    override func didReceiveMemoryWarning() {
+        log.warning("didReceiveMemoryWarning: \(type(of: self))")
+        super.didReceiveMemoryWarning()
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        log.verbose("prepare for \(segue.name)")
+        switch segue.identifier {
+        default:
+            log.debug("unexpected segue: \(segue.name)")
+        }
+    }
+}
+
+// MARK: - UITableViewDelegate
+
+extension RankingsFilterVC {
+
+    override func tableView(_ tableView: UITableView,
+                            heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+
+    override func tableView(_ tableView: UITableView,
+                            estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableView.automaticDimension
+    }
+}
+
+// MARK: - Private
+
+private extension RankingsFilterVC {
+
+    func configure() {
+    }
+}
