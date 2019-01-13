09cf0cca840dbdb01973a3183e17147b9d1bf52c
Alex Curylo <alex@alexcurylo.com>
2019-01-13 21:13:56 +0700

    Add Location struct

---
+++ MTP/application/Location.swift
@@ -0,0 +1,86 @@
+// @copyright Trollwerks Inc.
+
+import Foundation
+
+struct Location: Codable {
+    let Country: String? // not in staging
+    let CountryId: Int? // not in staging
+    let Location: String? // not in staging
+    let RegionIDnew: String? // not in staging
+    let RegionName: String? // not in staging
+    let active: String
+    let adminLevel: Int
+    let airports: String?
+    let countryId: Int
+    let countryName: String
+    let countVisitors: Int? // not in staging
+    let dateUpdated: Date? // not in staging
+    let distance: UncertainValue<Double, String> // Double in staging, String in production
+    let featuredImg: String?
+    let id: Int
+    let isMtpLocation: Int
+    let isUn: Int
+    let lat: UncertainValue<Double, String> // Double in staging, String in production
+    let latitude: String?
+    let locationName: String
+    let lon: UncertainValue<Double, String> // Double in staging, String in production
+    let longitude: String?
+    let rank: Int
+    let rankUn: Int
+    let regionId: Int
+    let regionName: String
+    let visitors: Int
+    let visitorsUn: Int
+    let weather: String?
+    let weatherhist: String?
+    let zoom: UncertainValue<Int, String> // Int in staging, String in production
+}
+
+extension Location: CustomStringConvertible {
+
+    public var description: String {
+        return "\(String(describing: countryName)) (\(String(describing: countryId)))"
+    }
+}
+
+extension Location: CustomDebugStringConvertible {
+
+    var debugDescription: String {
+        return """
+        < Location: \(description):
+        Country: \(String(describing: Country))
+        CountryId: \(String(describing: CountryId))
+        Location: \(String(describing: Location))
+        RegionIDnew: \(String(describing: RegionIDnew))
+        RegionName: \(String(describing: RegionName))
+        active: \(active)
+        admin_level: \(adminLevel)
+        airports: \(String(describing: airports))
+        countryId: \(String(describing: countryId))
+        countryName: \(String(describing: countryName))
+        count_visitors: \(String(describing: countVisitors))
+        dateUpdated: \(String(describing: dateUpdated))
+        distance: \(String(describing: distance))
+        is_mtp_location: \(isMtpLocation)
+        id: \(id)
+        is_un: \(isUn)
+        lat: \(String(describing: lat))
+        latitude: \(String(describing: latitude))
+        lon: \(String(describing: lon))
+        longitude: \(String(describing: longitude))
+        location_name: \(locationName)
+        rank: \(rank)
+        region_id: \(String(describing: regionId))
+        region_name: \(String(describing: regionName))
+        visitors: \(visitors)
+        /Location >
+        """
+    }
+}
+
+extension Location {
+
+    static var count: Int {
+        return gestalt.locations.count
+    }
+}
