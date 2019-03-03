99886e324d2db5fd23d7fc319c331a68b16f1865
Alex Curylo <alex@alexcurylo.com>
2019-03-03 10:16:16 +0700

    Refactor CLLocationCoordinate2D+Equatable

--- MTP/interface/locations/PlaceAnnotation.swift
+++ MTP/interface/locations/PlaceAnnotation.swift
@@ -63,17 +63,3 @@ final class PlaceAnnotation: NSObject, MKAnnotation {
         }
     }
 }
-
-extension CLLocationCoordinate2D: Equatable {
-
-    public static func == (lhs: CLLocationCoordinate2D,
-                           rhs: CLLocationCoordinate2D) -> Bool {
-        return lhs.latitude == rhs.latitude && lhs.longitude == rhs.longitude
-    }
-
-    static var zero = CLLocationCoordinate2D(latitude: 0, longitude: 0)
-
-    var isZero: Bool {
-        return self == .zero
-    }
-}
