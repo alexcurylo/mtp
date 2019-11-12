2e6d26701d7828a532f646e39a6fa2dcf74292cf
Alex Curylo <alex@alexcurylo.com>
2019-11-12 09:04:10 +0700

    Update docs

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -8,7 +8,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/).
 
 - Support for directions with Apple Maps, Google Maps, Citymapper, Transit, Lyft, Uber, Navigon, Waze, DB Navigator, Yandex.Navi and Moovit
 - Expanded visited map can be shared to Facebook
-- Support for editing and deleting posts and pictures from popup menu
+- Support for editing and deleting photos from popup menu
 
 ## [Version 1.2.0, Build 645] - 2019-10-10
 
--- MTP/sdks/UNUserNotificationCenterProtocol.swift
+++ MTP/sdks/UNUserNotificationCenterProtocol.swift
@@ -5,16 +5,30 @@ import UserNotifications
 /// Abstract UNUserNotificationCenter for testing
 protocol UNUserNotificationCenterProtocol: AnyObject {
 
+    /// Add notification request without completion handler
+    /// - Parameter request: UNNotificationRequest
     func add(_ request: UNNotificationRequest)
+    /// Add notification request
+    /// - Parameters:
+    ///   - request: UNNotificationRequest
+    ///   - completionHandler: Completion
     func add(_ request: UNNotificationRequest,
              withCompletionHandler completionHandler: ((Error?) -> Void)?)
+    /// Request notification status
+    /// - Parameter completionHandler: Completion
     func getNotificationStatus(completionHandler: @escaping (UNAuthorizationStatus) -> Void)
+    /// Request user authorization
+    /// - Parameters:
+    ///   - options: UNAuthorizationOptions
+    ///   - completionHandler: Completion
     func requestAuthorization(options: UNAuthorizationOptions,
                               completionHandler: @escaping (Bool, Error?) -> Void)
 }
 
 extension UNUserNotificationCenterProtocol {
 
+    /// Add request without completion handler
+    /// - Parameter request: UNNotificationRequest
     func add(_ request: UNNotificationRequest) {
         add(request, withCompletionHandler: nil)
     }
@@ -22,6 +36,8 @@ extension UNUserNotificationCenterProtocol {
 
 extension UNUserNotificationCenter: UNUserNotificationCenterProtocol {
 
+    /// Request notification status
+    /// - Parameter completionHandler: Completion
     func getNotificationStatus(completionHandler: @escaping (UNAuthorizationStatus) -> Void) {
         getNotificationSettings { completionHandler($0.authorizationStatus) }
     }
@@ -32,15 +48,18 @@ extension UNUserNotificationCenter: UNUserNotificationCenterProtocol {
 /// Stub for testing
 final class UNUserNotificationCenterStub: UNUserNotificationCenterProtocol {
 
+    /// :nodoc:
     func add(_ request: UNNotificationRequest,
              withCompletionHandler completionHandler: ((Error?) -> Void)?) {
         completionHandler?(nil)
     }
 
+    /// :nodoc:
     func getNotificationStatus(completionHandler: @escaping (UNAuthorizationStatus) -> Void) {
         completionHandler(.authorized)
     }
 
+    /// :nodoc:
     func requestAuthorization(options: UNAuthorizationOptions,
                               completionHandler: @escaping (Bool, Error?) -> Void) {
         completionHandler(true, nil)
